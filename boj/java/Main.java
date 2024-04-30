3218d79b1bc8b63d48306a4d4b464eddb9dddca0
nullisdefined <jaeuu.dev@gmail.com>
2024-04-30 16:21:03 +0900

    First commit

---
+++ boj/java/Main.java
@@ -0,0 +1,9 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        String answer = "";
+        System.out.println(answer+'c'+'d');
+    }
+}
