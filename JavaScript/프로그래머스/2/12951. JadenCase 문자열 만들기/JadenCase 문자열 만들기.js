ebce65aeda16b8362b665d4b7f74d94f7ed0c55f
Jaewoo Kim <164657817+nullisdefined@users.noreply.github.com>
2024-05-17 15:29:15 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- JavaScript/프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
+++ JavaScript/프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
@@ -1,5 +1,6 @@
 function solution(s) {
     let str = '';
+    // index가 아닌 charAt 메서드를 이용해서 빈 문자열로 인한 런타임을 방지함
     str = s.split(' ').map(word => {
         return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
     }).join(" ");
