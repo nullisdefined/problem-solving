7af45e5dfc6878a7dc0640d685a218ccdd59ddc2
Jaewoo Kim <164657817+nullisdefined@users.noreply.github.com>
2024-05-25 00:55:16 +0900

    [Silver III] Title: N과 M (8), Time: 212 ms, Memory: 21164 KB -BaekjoonHub

---
+++ Java/백준/Silver/15657. N과 M （8）/N과 M （8）.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static StringBuilder sb = new StringBuilder();
+    static int n, m;
+    static List<Integer> list = new ArrayList<>();
+    static int[] arr = new int[10];
+
+    static void func(int idx, int k) {
+        if(k == m) {
+            for(int i=0; i<m; ++i)
+                sb.append(arr[i]+" ");
+            sb.append("\n");
+            return;
+        }
+        for(int i=idx; i<n; ++i) {
+            arr[k] = list.get(i);
+            func(i, k+1);
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<n; ++i) {
+            list.add(Integer.parseInt(st.nextToken()));
+        }
+        list.sort(Comparator.naturalOrder());
+        func(0, 0);
+        System.out.println(sb);
+    }
+}
