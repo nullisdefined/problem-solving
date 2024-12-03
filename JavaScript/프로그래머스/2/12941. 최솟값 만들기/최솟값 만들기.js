efd31dd5c3a182cf0b4e4e36946cd4ca3aabc7c4
Jaewoo Kim <164657817+nullisdefined@users.noreply.github.com>
2024-12-03 14:13:19 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.77 ms, Memory: 33.4 MB -BaekjoonHub

--- JavaScript/프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.js
+++ JavaScript/프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.js
@@ -1,10 +1,11 @@
 function solution(A,B){
     var answer = 0;
-    A = A.sort((a, b) => a - b);
-    B = B.sort((a, b) => b - a);
-    // console.log(A, B);
-    A.forEach((e, i) => {
-        answer += e * B[i];
-    })
+    A.sort((a, b) => a - b);
+    B.sort((a, b) => b - a);
+    
+    for(let i = 0; i < A.length; i++) {
+        answer += A[i] * B[i];
+    }
+    
     return answer;
 }
