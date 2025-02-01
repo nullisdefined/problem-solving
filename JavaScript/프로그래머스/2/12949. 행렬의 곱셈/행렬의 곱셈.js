d1b4a8608036119d454d0376e7439630ad78d211
Jaewoo Kim <164657817+nullisdefined@users.noreply.github.com>
2025-02-01 10:28:08 +0900

    [level 2] Title: 행렬의 곱셈, Time: 6.31 ms, Memory: 38.4 MB -BaekjoonHub

--- JavaScript/프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.js
+++ JavaScript/프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.js
@@ -4,9 +4,12 @@ function solution(arr1, arr2) {
         answer.push(new Array(arr2[0].length).fill(0));
     }
     
+    // 첫 번째 for: 첫 번째 행렬의 행 정하기
     for(let i=0; i<arr1.length; ++i) {
+        // 두 번째 for: 두 번째 행렬의 열 정하기
         for(let j=0; j<arr2[0].length; ++j) {
             let value = 0;
+            // 세 번째 for: 곱할 원소 인덱스 정하기
             for(let k=0; k<arr1[0].length; ++k) {
                 value += arr1[i][k] * arr2[k][j];
             }
